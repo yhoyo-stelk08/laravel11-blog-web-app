5e94dcff8f1235004cf6640a789178fd69b1aee3
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-03 00:07:34 +0800

    feat: format the create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,20 +4,36 @@
 
 @section('content')
   <div
-    class="mx-auto flex w-auto max-w-2xl flex-col justify-start gap-4 rounded-lg border border-solid border-amber-300 p-4 shadow-lg">
+    class="mx-auto flex w-auto max-w-2xl flex-col justify-start gap-4 rounded-lg border border-solid border-amber-300 p-4 shadow-lg"
+  >
     <h1 class="text-center text-2xl font-bold">Create A Post</h1>
-    <form action="" method="POST">
+    <form
+      action="{{ route('posts.store') }}"
+      method="POST"
+    >
       <div class="m-4 flex items-center justify-start gap-8">
         <label for="title">Post Title</label>
-        <input type="text" id="title" name="title" class="w-3/4 rounded-md border-2 p-2">
+        <input
+          type="text"
+          id="title"
+          name="title"
+          class="w-3/4 rounded-md border-2 p-2"
+        >
       </div>
       <div class="m-4 flex items-start justify-start gap-8">
         <label for="content">Content</label>
-        <textarea rows="6" name="content" id="content" class="ml-2 w-3/4 rounded-md border-2 p-2"></textarea>
+        <textarea
+          rows="6"
+          name="content"
+          id="content"
+          class="ml-2 w-3/4 rounded-md border-2 p-2"
+        ></textarea>
       </div>
       <div class="flex items-center justify-end">
-        <button type="submit"
-          class="my-2 mr-16 rounded-md border-blue-200 bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">Submit</button>
+        <button
+          type="submit"
+          class="my-2 mr-16 rounded-md border-blue-200 bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+        >Submit</button>
       </div>
     </form>
   </div>
