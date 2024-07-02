e3a6aea6ca7cd550f883a179641fb24453cf403c
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-02 21:10:26 +0800

    feat: create new view layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  @vite('resources/css/app.css')
-  <title>Laravel Blog - @yield('title')</title>
-</head>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Laravel Blog - @yield('title')</title>
+  </head>
 
-<body>
-  @yield('content')
-</body>
+  <body class="container mx-auto my-10 max-w-xl">
+    <header>
+      <h1>
+        @yield('header')
+      </h1>
+    </header>
+    <section>
+
+      @yield('content')
+    </section>
+  </body>
 
 </html>
