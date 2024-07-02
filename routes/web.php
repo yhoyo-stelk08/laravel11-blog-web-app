6a57677ce9b4f738f1247884a86dce67baeb4e5f
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-02 21:21:19 +0800

    feat: create new route.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
+
+Route::resource('posts', PostsController::class)->only(['index','show']);
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
