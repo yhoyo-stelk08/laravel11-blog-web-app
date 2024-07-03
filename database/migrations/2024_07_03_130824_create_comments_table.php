648ac1a459fb01680d9fb347df6111d751cc0130
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-03 13:15:58 +0800

    feat: create foreign key relation in migration file for comment.

--- database/migrations/2024_07_03_130824_create_comments_table.php
+++ database/migrations/2024_07_03_130824_create_comments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,11 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('content');
+            $table->unsignedBigInteger('blog_post_id')->index();
             $table->timestamps();
+
+            $table->foreign('blog_post_id')->references('id')->on('blog_posts');
         });
     }
 
