9c83e80c0c6631af4e2d51563e5f8e32dbc4a77c
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-03 17:51:07 +0800

    feat: displaying comments. - remove edit and delete post button. - displaying comments for post. - do some styling.
     Changes to be committed:
    	modified:   resources/views/posts/show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,25 +19,30 @@
     <p class="mb-4">
       {{ $post->content }}
     </p>
-    <div class="flex items-center justify-start gap-4 py-4">
-      <a
-        href="{{ route('posts.edit', ['post' => $post->id]) }}"
-        class="rounded bg-blue-400 px-4 py-2 text-white hover:bg-blue-600"
-      >Edit Post</a>
-      <form
-        action="{{ route('posts.destroy', ['post' => $post->id]) }}"
-        method="POST"
-      >
-        @csrf
-        @method('DELETE')
-        <input
-          type="submit"
-          value="Delete Post"
-          class="rounded bg-red-400 px-4 py-2 text-white hover:bg-red-600"
-        >
-      </form>
-
+    <div class="flex flex-row items-center justify-start gap-4">
+      <label>{{ $post->created_at->diffForHumans() }}</label>
     </div>
+    <div class="my-4 flex flex-col items-start justify-start">
+      <h1>Comments</h1>
+      @forelse ($post->comments as $comment)
+        <div class="my-2 w-full bg-slate-100">
+          <p class="mx-2 my-2 p-2">
+            {{ $comment->content }}
+          </p>
+          <p class="mx-2 my-2 p-2 text-xs">
+
+            commented at {{ $comment->created_at->diffForHumans() }}
+          </p>
+        </div>
+
+      @empty
+        <div class="my-2 w-full bg-slate-100">
+
+          <h1 class="p-2 text-xs">No Comments Yet..!!</h1>
+        </div>
+      @endforelse
+    </div>
+
 
   </div>
 
