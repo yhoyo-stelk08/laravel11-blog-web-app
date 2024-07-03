5e76a3fe11fa6a506e96e6b3110a791b3cb7d406
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-03 12:41:14 +0800

    feat: create one to one relationship between Author and Profile.

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
