f2dc76a82f19d6e7e53f1ef0a124f1d609ba7e28
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-03 09:13:12 +0800

    feat: displaying flash messages

--- resources/views/layouts/posts.blade.php
+++ resources/views/layouts/posts.blade.php
@@ -3,13 +3,24 @@
 
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    >
+    <meta
+      http-equiv="X-UA-Compatible"
+      content="ie=edge"
+    >
     @vite('resources/css/app.css')
     <title>Laravel Blog - @yield('header')</title>
   </head>
 
   <body class="container p-8">
+    @if (session('status'))
+      <div class="bg-red-400 text-white">
+        {{ session('status') }}
+      </div>
+    @endif
     @yield('content')
   </body>
 
