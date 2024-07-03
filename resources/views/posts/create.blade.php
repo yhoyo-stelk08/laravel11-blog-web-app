ba583b96208407be590d9ee71da13940bd765926
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-07-03 09:15:22 +0800

    feat: make the input field retain the old value.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -18,6 +18,7 @@
           type="text"
           id="title"
           name="title"
+          value="{{ old('title') }}"
           class="w-3/4 rounded-md border-2 p-2"
         >
       </div>
@@ -33,7 +34,9 @@
           name="content"
           id="content"
           class="ml-2 w-3/4 rounded-md border-2 p-2"
-        ></textarea>
+        >
+        {{ old('content') }}
+        </textarea>
       </div>
       @error('content')
         <div class="items-centerp-2 ml-32 w-2/4 text-xs text-red-400">
